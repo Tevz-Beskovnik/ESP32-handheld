9ef9776b594c45bcbebf256e42eae7fcc1d926c1
tevzi2 <tevz.beskovnik@gmail.com>
2022-11-30 11:12:10 +0100

    SPI driver (TODO)

--- lib/Drivers/SPI_driver.hpp
+++ lib/Drivers/SPI_driver.hpp
@@ -5,6 +5,12 @@
 #include <SPI.h>
 #include <Arduino.h>
 
+/**
+ * TODO: Implement this shit that uses direct eps32 libs (line 317): 
+ * https://github.com/Inteform/esp32-lora-library/blob/master/components/lora/lora.c
+ * (should work way faster)
+*/
+
 class SPIDriver {
     public:
         SPIDriver(uint8_t pinClk, uint8_t pinMosi, uint8_t pinMiso, uint8_t pinCs, uint8_t pBitOrder, uint32_t clock);
